228903c6df0a0902f26c803dd7a16c1bbca85b96
Andrey_Schulz <andreygorb040693@gmail.com>
2022-11-09 19:22:01 +0200

    Update MovieDetails.jsx

--- src/Pages/MovieDetails.jsx
+++ src/Pages/MovieDetails.jsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
-import { useParams, Outlet } from 'react-router-dom';
+import { useParams, Outlet, Link } from 'react-router-dom';
 import { searchMovieToIdApi } from '../utils/MovieApi';
 const MovieDetails = () => {
+  const START_URL = 'https://image.tmdb.org/t/p/w500/';
   const [details, setDetails] = useState([]);
   const { movieId } = useParams();
   // const movieInfo = searchMovieToIdApi(movieId);
@@ -10,13 +11,26 @@ const MovieDetails = () => {
     searchMovieToIdApi(movieId).then(data => setDetails(data));
   }, [movieId]);
   console.log(details);
-  const { title, backdrop_path, poster_path } = details;
+  const { title, poster_path, vote_average, overview, genres } = details;
   return (
     <>
+      <img src={`${START_URL}${poster_path}`} alt={title}></img>
       <h1>{title}</h1>
-      <img href={poster_path}></img>
-
+      <p>User score : {Math.round(vote_average * 10)}%</p>
+      <h2>Overview</h2>
+      <p>{overview}</p>
       <h2>Ganres</h2>
+      <ul>{genres && genres.map(genre => <li>{genre.name}</li>)}</ul>
+
+      <h3>Additional information</h3>
+      <ul>
+        <li>
+          <Link to="cast" />
+        </li>
+        <li>
+          <Link to="cast" />
+        </li>
+      </ul>
 
       <Outlet />
     </>
